37f7449e73ff016379fc861a53f6141b708c0610
jiten0709 <parmar.jiten.microsoft@gmail.com>
2024-04-19 18:01:41 +0530

    added test url

--- main.py
+++ main.py
@@ -59,7 +59,46 @@ def predict():
 
     return jsonify(top3_predictions)
 
+@app.route('/test', methods=['POST'])
+def test():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image file'}), 400
 
+    # Access the uploaded file
+    image_file = request.files['image']
+    # Convert the file stream to bytes
+    image_bytes = image_file.read()
+    # Convert the bytes to a PIL Image object
+    img = image.open(BytesIO(image_bytes))
+    # Resize the image to the target size
+    img = img.resize((299, 299))
+    # Convert the image to a NumPy array
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+
+    img_array = np.expand_dims(img_array, axis=0) / 255.
+
+    # Assuming model is already loaded
+    predictions = model.predict(img_array)
+    instance_predictions = predictions[0]
+    flattened_predictions = instance_predictions.flatten()
+    sorted_indices = np.argsort(flattened_predictions)[::-1]
+
+    # Initialize an empty list to store the sorted predictions
+    sorted_predictions = []
+
+    for index in sorted_indices:
+        class_name = list(class_labels.keys())[index]
+        # Convert float32 to float, multiply by 100 to get percentage, and round to 2 decimal places
+        probability = round(float(flattened_predictions[index]) * 100, 2)
+        # Store the class name and probability as a tuple
+        sorted_predictions.append((class_name, probability))
+    
+    # Convert the list of tuples into a dictionary
+    top3_predictions = dict(sorted_predictions[:3])
+
+    print(top3_predictions)
+
+    return jsonify(top3_predictions)
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
